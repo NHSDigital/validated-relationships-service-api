f61f7710f405d7a1790a4b58799dd5aae56a1d33
JackPlowman <62281988+JackPlowman@users.noreply.github.com>
2024-04-04 10:10:51 +0100

    Add more unit tests and fix existing test

--- sandbox/api/app.py
+++ sandbox/api/app.py
@@ -10,6 +10,17 @@ basicConfig(level=INFO, format="%(asctime)s - %(message)s")
 logger = getLogger(__name__)
 
 
+@app.route("/_status", methods=["GET"])
+@app.route("/_ping", methods=["GET"])
+@app.route("/health", methods=["GET"])
+def health() -> dict:
+    """Health check endpoint"""
+    return {
+        "status": "online",
+        "message": "Validated Relationships Service Sandbox is running",
+    }
+
+
 @app.route("/RelatedPerson", methods=["GET"])
 def related_persons() -> Union[dict, tuple]:
     """Sandbox API for GET /RelatedPerson
--- sandbox/api/tests/test_app.py
+++ sandbox/api/tests/test_app.py
@@ -2,21 +2,51 @@ from unittest.mock import MagicMock, patch
 
 import pytest
 
-from ..app import app
+FILE_PATH = "sandbox.api.app"
 
 
-@pytest.fixture()
-def client() -> object:
-    """Create a test client for the app."""
-    return app.test_client()
-
-
-@patch("sandbox.api.app.get_response")
-def test_related_person(mock_get_response: MagicMock, client: object) -> None:
-    """"""
-    # Arrange
-    mock_get_response.return_value = {"data": "mocked"}
+@pytest.mark.parametrize("endpoint", ["/_status", "/_ping", "/health"])
+def test_health_check(client: object, endpoint: str) -> None:
+    """Test health check endpoints."""
     # Act
-    client.get("/RelatedPerson?identifier=1234")
+    response = client.get(endpoint)
     # Assert
-    mock_get_response.assert_called_once_with("./api/responses/RelatedPerson.json")
+    assert response.status_code == 200
+    assert response.json == {
+        "status": "online",
+        "message": "Validated Relationships Service Sandbox is running",
+    }
+
+
+@patch(f"{FILE_PATH}.get_response")
+def test_related_person__identifier_only(
+    mock_get_response: MagicMock, client: object
+) -> None:
+    """Test related_persons endpoint with identifier only."""
+    # Arrange
+    mock_get_response.return_value = expected_body = {"data": "mocked"}
+    # Act
+    response = client.get("/RelatedPerson?identifier=1234567890")
+    # Assert
+    mock_get_response.assert_called_once_with(
+        "./api/responses/GET_RelatedPerson/identifier.json"
+    )
+    assert response.status_code == 200
+    assert response.json == expected_body
+
+
+@patch(f"{FILE_PATH}.get_response")
+def test_related_person__identifier_and_patient(
+    mock_get_response: MagicMock, client: object
+) -> None:
+    """Test related_persons endpoint with identifier and patient."""
+    # Arrange
+    mock_get_response.return_value = expected_body = {"data": "mocked"}
+    # Act
+    response = client.get("/RelatedPerson?identifier=1234567890&patient=0987654321")
+    # Assert
+    mock_get_response.assert_called_once_with(
+        "./api/responses/GET_RelatedPerson/identifier_and_patient.json"
+    )
+    assert response.status_code == 200
+    assert response.json == expected_body
