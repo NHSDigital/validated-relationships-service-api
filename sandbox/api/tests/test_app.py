804af0499ac4a35691d90c5e504ddad33a7c08fb
kamran-iqbal4-NHSD <144686155+kamran-iqbal4-NHSD@users.noreply.github.com>
2025-03-20 15:36:15 +0000

    Updated sandbox with patch endpoint

--- sandbox/api/tests/test_app.py
+++ sandbox/api/tests/test_app.py
@@ -8,6 +8,12 @@ from sandbox.api.constants import (
     POST_CONSENT__SUCCESS,
     POST_CONSENT__DUPLICATE_RELATIONSHIP_ERROR,
     POST_CONSENT__PERFORMER_IDENTIFIER_ERROR,
+    PATCH_CONSENT__SUCCESS,
+    PATCH_CONSENT__INVALID_PATCH_FORMAT,
+    PATCH_CONSENT__INVALID_PATH,
+    PATCH_CONSENT__INVALID_STATUS_CODE,
+    PATCH_CONSENT__RESOURCE_NOT_FOUND,
+    PATCH_CONSENT__INVALID_STATE_TRANSITION,
 )
 
 from .conftest import (
@@ -232,6 +238,48 @@ def test_post_consent_when_valid_returns_success(
         )
     else:
         mock_generate_response_from_example.assert_called_once_with(response_file_name, status_code)
+    assert response.status_code == status_code
+    assert response.json == loads(mocked_response.get_data(as_text=True))
+
+
+@pytest.mark.parametrize(
+    "nhs_num,response_file_name,status_code",
+    [
+        ("c6f48e4d", PATCH_CONSENT__SUCCESS, 200),
+        ("0c56a594", PATCH_CONSENT__SUCCESS, 200),
+        ("b02ea26c", PATCH_CONSENT__SUCCESS, 200),
+        ("3a2679eb", PATCH_CONSENT__INVALID_PATCH_FORMAT, 400),
+        ("94df7c8f", PATCH_CONSENT__INVALID_PATH, 400),
+        ("2a7b736d", PATCH_CONSENT__INVALID_STATUS_CODE, 422),
+        ("6fb4361b", PATCH_CONSENT__INVALID_STATE_TRANSITION, 422),
+        ("xxxxxxxx", PATCH_CONSENT__RESOURCE_NOT_FOUND, 404),
+        ("12345678", PATCH_CONSENT__RESOURCE_NOT_FOUND, 404),
+    ],
+)
+@patch(f"{APP_FILE_PATH}.generate_response_from_example")
+def test_patch_consent_on_request_returns_expected_response(
+    mock_generate_response_from_example: MagicMock,
+    response_file_name: str,
+    nhs_num: str,
+    client: object,
+    status_code: int,
+) -> None:
+    """
+    Function: app.patch_consent
+    Scenario: Valid consent is PATCHed to /consent
+    Expected Behavior: Request is handled and appropriate response is returned
+    """
+    # Arrange
+    mock_generate_response_from_example.return_value = mocked_response = Response(
+        dumps({"data": "mocked"}),
+        status=status_code,
+        content_type="application/json",
+    )
+    # Act
+    json = [{"op": "replace", "path": "/status", "value": "inactive"}]
+    response = client.patch(CONSENT_API_ENDPOINT + f"/{nhs_num}", json=json)
+    # Assert
+    mock_generate_response_from_example.assert_called_once_with(response_file_name, status_code)
     assert response.status_code == status_code
     assert response.json == loads(mocked_response.get_data(as_text=True))
 
