d715817e401d4d1f5c1b48e6e6bbb8524238c297
Ellie Bound <175816742+ellie-bound1-NHSD@users.noreply.github.com>
2025-04-03 13:16:42 +0100

    NPA-4599: Updates requested after PR

--- proxies/live/apiproxy/resources/py/check-app-enabled-endpoint.py
+++ proxies/live/apiproxy/resources/py/check-app-enabled-endpoint.py
@@ -1,12 +1,12 @@
-path_suffix = flow.getVariable("proxy.pathsuffix")
-request_verb = flow.getVariable("request.verb")
+path_suffix = flow.getVariable("proxy.pathsuffix").lower()
+request_verb = flow.getVariable("request.verb").lower()
 
 requested_endpoint = (path_suffix, request_verb)
 
 
 auth_forbidden = requested_endpoint in [
-    ("/FHIR/R4/RelatedPerson", "GET"),
-    ("/FHIR/R4/QuestionnaireResponse", "POST"),
+    ("/fhir/r4/relatedperson", "get"),
+    ("/fhir/r4/questionnaireresponse", "post"),
 ]
 
 flow.setVariable("app_auth_forbidden", auth_forbidden)
--- proxies/live/apiproxy/resources/py/check-user-enabled-endpoint.py
+++ proxies/live/apiproxy/resources/py/check-user-enabled-endpoint.py
@@ -1,13 +1,13 @@
-auth_level = flow.getVariable("accesstoken.auth_level")
-path_suffix = flow.getVariable("proxy.pathsuffix")
-request_verb = flow.getVariable("request.verb")
+auth_level = flow.getVariable("accesstoken.auth_level").lower()
+path_suffix = flow.getVariable("proxy.pathsuffix").lower()
+request_verb = flow.getVariable("request.verb").lower()
 
 requested_resource = (path_suffix, request_verb)
 
 if auth_level == "p9":
-    blocked_resources = [("/FHIR/R4/Consent", "POST"), ("/FHIR/R4/Consent", "PATCH")]
+    blocked_resources = [("/fhir/r4/consent", "post"), ("/fhir/r4/consent", "patch")]
 elif auth_level == "all3":
-    blocked_resources = [("/FHIR/R4/QuestionnaireResponse", "POST")]
+    blocked_resources = [("/fhir/r4/questionnaireresponse", "post")]
 else:
     blocked_resources = []
 
