0cd2707665f9880b229830228431b1e721b59a27
Ellie Bound <175816742+ellie-bound1-NHSD@users.noreply.github.com>
2025-05-23 14:23:09 +0100

    NPA-4284: Add python script to translate request headers

---
+++ proxies/live/apiproxy/resources/py/translate-request-headers.py
@@ -0,0 +1,15 @@
+# Access request headers dictionary
+request_headers = request.headers
+
+# Map of lowercase header name to desired parcel case header name
+request_header_translation = {
+    "x-request-id": "X-Request-ID",
+    "x-correlation-id": "X-Correlation-ID"
+}
+
+# Loop through request headers
+for key, value in request_headers.items():
+    key = key.lower()
+    desired_name = request_header_translation.get(key)
+    if desired_name:
+        flow.setVariable(f"request.header.{desired_name}", value)
