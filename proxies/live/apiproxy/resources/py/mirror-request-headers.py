b969d2efe903ab5ee93aac431eb192c6b914b40c
Ellie Bound <175816742+ellie-bound1-NHSD@users.noreply.github.com>
2025-05-27 14:08:04 +0100

    NPA-8242: Working mirror request headers for unhappy path

--- proxies/live/apiproxy/resources/py/mirror-request-headers.py
+++ proxies/live/apiproxy/resources/py/mirror-request-headers.py
@@ -1,6 +1,10 @@
-# Access original request headers dictionary
-request_headers = JSON.parse(flow.getVariable("original.headers"))
-
-# Loop through request headers and set them as response headers
-for key, value in request_headers.items():
-    flow.setVariable("response.header.{0}".format(key), value)
+translated_header_names = ["X-Request-ID", "X-Correlation-ID"]
+for translated_header_name in translated_header_names:
+    # Access original request headers dictionary
+    original_request_header = flow.getVariable("original_request_header." + translated_header_name)
+    if original_request_header:
+        original_request_header = original_request_header.split("=")
+        # Remove original response header
+        flow.removeVariable("response.header." + translated_header_name)
+        # Mirror original request header
+        flow.setVariable("response.header." + original_request_header[0], original_request_header[1])
--- proxies/live/apiproxy/resources/py/translate-request-headers.py
+++ proxies/live/apiproxy/resources/py/translate-request-headers.py
@@ -10,17 +10,18 @@ def get_request_headers():
 # Access request headers dictionary
 request_headers = get_request_headers()
 
-# Store copy of original request headers
-flow.setVariable("original.headers", str(request_headers))
-
 # Map of lowercase header name to desired parcel case header name
 request_header_translation = {"x-request-id": "X-Request-ID", "x-correlation-id": "X-Correlation-ID"}
 
 # Loop through request headers
-for key, value in request_headers.items():
-    desired_name = request_header_translation.get(key.lower())
-    if desired_name:
-        # Remove original header
-        flow.removeVariable("request.header." + key)
+for original_header_name, header_value in request_headers.items():
+    desired_header_name = request_header_translation.get(original_header_name.lower())
+    if desired_header_name:
+        # Store original header against the desired name for mirroring in response
+        flow.setVariable(
+            "original_request_header." + desired_header_name, "%s=%s" % (original_header_name, header_value)
+        )
+        # Remove original request header
+        flow.removeVariable("request.header." + original_header_name)
         # Set header with correct casing
-        flow.setVariable("request.header." + desired_name, value)
+        flow.setVariable("request.header." + desired_header_name, header_value)
